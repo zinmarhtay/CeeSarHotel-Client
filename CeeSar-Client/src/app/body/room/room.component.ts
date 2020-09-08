5a4d580438ba16bad2a3ae9f790e3b0aa9f0a6eb
zin <zin50887@gmail.com>
2020-09-08 16:42:36 +0630

    adding Room Component of HTML and CSS

--- CeeSar-Client/src/app/body/room/room.component.ts
+++ CeeSar-Client/src/app/body/room/room.component.ts
@@ -3,8 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-room',
   templateUrl: './room.component.html',
-  styles: [
-  ]
+  styleUrls: ['./room.component.css']
 })
 export class RoomComponent implements OnInit {
 
